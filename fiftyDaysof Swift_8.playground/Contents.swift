4d6fa7ca3827b0f2aa92a6e7bf1e4b76343bb9a3
Enes Kaya <eng.eneskaya@gmail.com>
2023-02-20 15:50:50 +0300

    no message

---
+++ fiftyDaysof Swift_8.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+
+class ServiceHelper {
+    
+    func fetchDataService(){
+        
+        
+    }
+    
+    func saveDataLocal(){
+        
+        
+    }
+    
+}
+
+
+var serviceHelper = ServiceHelper()
+
+serviceHelper.fetchDataService()
+
+
